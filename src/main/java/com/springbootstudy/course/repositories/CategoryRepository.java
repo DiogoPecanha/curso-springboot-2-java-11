4ee199d1c6393e11fabe887c4e733c6a81517f4c
Emmanuelle Ferreira <emmanuelle@MacBook-Air-de-Emmanuelle.local>
2020-10-04 20:27:24 -0300

    Category - Parte 1

---
+++ src/main/java/com/springbootstudy/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.springbootstudy.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.springbootstudy.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
